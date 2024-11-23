5fd7a1dd597536b5935a3a454c90d806f6edb772
zhangjianlong1990 <1107937738@qq.com>
2024-11-23 15:00:04 +0800

    站点数据增加通知，测试

--- plugins.v2/sitedata/__init__.py
+++ plugins.v2/sitedata/__init__.py
@@ -33,7 +33,7 @@ class SiteData(_PluginBase):
     # 插件图标
     plugin_icon = "statistic.png"
     # 插件版本
-    plugin_version = "1.3.1"
+    plugin_version = "1.4.0"
     # 插件作者
     plugin_author = "jianlongzhang1990,lightolly,jxxghp"
     # 作者主页
