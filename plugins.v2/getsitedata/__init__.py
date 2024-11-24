9a6b4f2c63ae33b7ae8964ecdaa812f4060d6055
zhangjianlong1990 <1107937738@qq.com>
2024-11-24 18:02:29 +0800

    站点数据刷新时触发通知

--- plugins.v2/getsitedata/__init__.py
+++ plugins.v2/getsitedata/__init__.py
@@ -37,7 +37,7 @@ class GetSiteData(_PluginBase):
     # 插件图标
     plugin_icon = "statistic.png"
     # 插件版本
-    plugin_version = "1.0.3"
+    plugin_version = "1.0.4"
     # 插件作者
     plugin_author = "jianlongzhang1990,lightolly,jxxghp"
     # 作者主页
@@ -871,6 +871,7 @@ class GetSiteData(_PluginBase):
             self.post_message(channel=event.event_data.get("channel"),
                               title="站点数据刷新完成！", userid=event.event_data.get("user"))
 
+    @eventmanager.register(EventType.SiteRefreshed)
     def notify(self, event):
         # 获取数据
         today, stattistic_data, yesterday_sites_data = self.__get_data()
